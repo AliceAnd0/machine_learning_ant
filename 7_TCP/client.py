fbd6291f997f574e215e720f08a0a485310ad4e4
AliceAnd0 <aliseliddell90@gmail.com>
2024-04-17 13:08:18 +0300

    gigigi

--- 7_TCP/client.py
+++ 7_TCP/client.py
@@ -7,6 +7,18 @@ import time, os
 import base64
 import threading, wave, pickle,struct
 # For details visit pyshine.com
+
+# path=os.path.join(os.path.abspath(os.curdir) , 'my_model.onnx')
+caffemodel=os.path.join(os.path.abspath(os.curdir) , 'MobileNetSSD_deploy.caffemodel')
+txt=os.path.join(os.path.abspath(os.curdir) , 'MobileNetSSD_deploy.prototxt.txt')
+args_confidence = 0.2
+
+# initialize the list of class labels 
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+
 BUFF_SIZE = 65536
 
 BREAK = False
@@ -51,24 +63,30 @@ def video_stream():
 								, size=(32, 32), mean= (128,128,128), swapRB=True)
 		cv2.imshow("Cropped image", cv2.resize(frame, (32, 32)))
 
+		for i in np.arange(0, detections.shape[2]):
+			confidence = detections[0, 0, i, 2]
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+			if confidence > args_confidence:
+        	# extract the index of the class label from the
+        	# `detections`, then compute the (x, y)-coordinates of
+        	# the bounding box for the object
+				idx = int(detections[0, 0, i, 1])
+				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				(startX, startY, endX, endY) = box.astype("int")
+
+            # draw the prediction on the frame
+				label = "{}: {:.2f}%".format(CLASSES[idx],confidence * 100)
+				cv2.rectangle(frame, (startX, startY), (endX, endY),COLORS[idx], 2)
+				y = startY - 15 if startY - 15 > 15 else startY + 15
+				cv2.putText(frame, label, (startX, y),cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+    
 		# pass the blob through the network and obtain the detections and
 		# predictions
 		net.setInput(blob)
 		detections = net.forward()
 		print(list(zip(CLASSES,detections[0])))
-		# loop over the detections
-
-			# extract the confidence (i.e., probability) associated with
-			# the prediction
-		confidence = abs(detections[0][0]-detections[0][1])
-		print("confidence = ", confidence)
-			# filter out weak detections by ensuring the `confidence` is
-			# greater than the minimum confidence
-		if (confidence > args_confidence) :
-			
-			class_mark=np.argmax(detections)
-			cv2.putText(frame, CLASSES[class_mark], (30,30),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (242, 230, 220), 2)
-
+		
 		cv2.imshow("RECEIVING VIDEO",frame)
 		key = cv2.waitKey(1) & 0xFF
 	
